8162ee9516f5be06f67459c385e55d940cacbc64
Xander Koen <xkoen@hotmail.com>
2026-08-28 03:05:09 +0200

    Truckin it update
    Trucking it page got updated with some goodies, also changed the carousel to have a 16/9 ratio to better fit the pictures they were showing

--- JS/carousel.js
+++ JS/carousel.js
@@ -1,92 +1,33 @@
-window.onload = start
+window.addEventListener('load', () => {
+    const imageSlides = document.getElementsByClassName('imageSlides');
+    const circles = document.getElementsByClassName('circle');
+    const leftArrow = document.getElementById('leftArrow');
+    const rightArrow = document.getElementById('rightArrow');
 
-    let imageSlides
-    let circles
-    let leftArrow
-    let rightArrow
-    let counter
+    if (!imageSlides.length || circles.length < imageSlides.length || !leftArrow || !rightArrow) {
+        return;
+    }
 
-function start() {
-    // IMAGE SLIDES & CIRCLES ARRAYS, & COUNTER
-    imageSlides = document.getElementsByClassName('imageSlides');
-    circles = document.getElementsByClassName('circle');
-    leftArrow = document.getElementById('leftArrow');
-    rightArrow = document.getElementById('rightArrow');
-    counter = 0;
+    let currentIndex = 0;
 
+    function showSlide(index) {
+        currentIndex = (index + imageSlides.length) % imageSlides.length;
 
-// HIDE ALL IMAGES FUNCTION
-    function hideImages() {
         for (let i = 0; i < imageSlides.length; i++) {
-            imageSlides[i].classList.remove('reveal');
+            imageSlides[i].classList.toggle('reveal', i === currentIndex);
+            circles[i].classList.toggle('dot', i === currentIndex);
         }
     }
 
-// REMOVE ALL DOTS FUNCTION
-    function removeDots() {
-        for (let i = 0; i < imageSlides.length; i++) {
-            circles[i].classList.remove('dot');
-        }
+    showSlide(0);
+    let imageSlideshowInterval = setInterval(() => showSlide(currentIndex + 1), 4000);
+
+    function moveSlide(step) {
+        clearInterval(imageSlideshowInterval);
+        showSlide(currentIndex + step);
+        imageSlideshowInterval = setInterval(() => showSlide(currentIndex + 1), 4000);
     }
 
-// SINGLE IMAGE LOOP/CIRCLES FUNCTION
-    function imageLoop() {
-        let currentImage = imageSlides[counter];
-        let currentDot = circles[counter];
-        currentImage.classList.add('reveal');
-        removeDots();
-        currentDot.classList.add('dot');
-        counter++;
-    }
-
-// LEFT & RIGHT ARROW FUNCTION & CLICK EVENT LISTENERS
-    function arrowClick(e) {
-        let target = e.target;
-        if (target === leftArrow) {
-            clearInterval(imageSlideshowInterval);
-            hideImages();
-            removeDots();
-            if (counter === 1) {
-                counter = (imageSlides.length - 1);
-                imageLoop();
-                imageSlideshowInterval = setInterval(slideshow, 4000);
-            } else {
-                counter--;
-                counter--;
-                imageLoop();
-                imageSlideshowInterval = setInterval(slideshow, 4000);
-            }
-        } else if (target === rightArrow) {
-            clearInterval(imageSlideshowInterval);
-            hideImages();
-            removeDots();
-            if (counter === imageSlides.length) {
-                counter = 0;
-                imageLoop();
-                imageSlideshowInterval = setInterval(slideshow, 4000);
-            } else {
-                imageLoop();
-                imageSlideshowInterval = setInterval(slideshow, 4000);
-            }
-        }
-    }
-
-    leftArrow.addEventListener('click', arrowClick);
-    rightArrow.addEventListener('click', arrowClick);
-
-
-// IMAGE SLIDE FUNCTION
-    function slideshow() {
-        if (counter < imageSlides.length) {
-            imageLoop();
-        } else {
-            counter = 0;
-            hideImages();
-            imageLoop();
-        }
-    }
-
-// SHOW FIRST IMAGE, & THEN SET & CALL SLIDE INTERVAL
-    setTimeout(slideshow, 1000);
-    let imageSlideshowInterval = setInterval(slideshow, 4000);
-}
+    leftArrow.addEventListener('click', () => moveSlide(-1));
+    rightArrow.addEventListener('click', () => moveSlide(1));
+});
